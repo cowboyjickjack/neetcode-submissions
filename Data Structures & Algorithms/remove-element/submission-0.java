b9da1c7ffd634639e0aa6a304a8d81d8c0a14ba8
Gabriella Martinez <124517840+cowboyjickjack@users.noreply.github.com>
2026-09-04 15:33:42 -0500

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        if(nums.length == 0) return 0; // if nothing, return nothing
+
+        int validSize = 0; // front of array
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != val){
+                nums[validSize] = nums[i];
+                validSize++; // put valid elements to the front
+            } else {
+
+            }
+        }
+
+        return validSize;
+    }
+}
